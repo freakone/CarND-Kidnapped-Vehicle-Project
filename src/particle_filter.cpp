ed96d63eb8a1b51277a380dca378cac8b77c0bdb
Kamil Górski <kamil@freakone.pl>
2017-12-05 17:01:06 +0100

    Method updateWeights works

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -26,7 +26,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 	
-	num_particles = 50;
+	num_particles = 100;
 
 	std::default_random_engine generator;
 	std::normal_distribution<double> gauss_x(x, std[0]);
@@ -60,24 +60,24 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	std::normal_distribution<double> gauss_y(0.0, std_pos[1]);
 	std::normal_distribution<double> gauss_t(0.0, std_pos[2]);
 
-	for (int i = 0;  i < num_particles; i++) {
+	for (auto& p : particles) {
 
 		if(fabs(yaw_rate) < std::numeric_limits<double>::epsilon()) //if yaw is zero
 		{
 			const double vdt = velocity * delta_t;
 
-			particles[i].x +=  vdt * cos(particles[i].theta) + gauss_x(generator);
-			particles[i].y += vdt * sin(particles[i].theta) + gauss_y(generator);
-			particles[i].theta += gauss_t(generator);	
+			p.x +=  vdt * cos(p.theta) + gauss_x(generator);
+			p.y += vdt * sin(p.theta) + gauss_y(generator);
+			p.theta += gauss_t(generator);	
 		}
 		else
 		{
-	      	const double phi = particles[i].theta + delta_t * yaw_rate;
+	      	const double phi = p.theta + delta_t * yaw_rate;
 			const double vy = velocity / yaw_rate;
 
-			particles[i].x += vy * (sin(phi) -  sin(particles[i].theta)) + gauss_x(generator);
-			particles[i].y +=  vy * (cos(particles[i].theta) - cos(phi)) + gauss_y(generator);
-			particles[i].theta = phi + gauss_t(generator);
+			p.x += vy * (sin(phi) -  sin(p.theta)) + gauss_x(generator);
+			p.y +=  vy * (cos(p.theta) - cos(phi)) + gauss_y(generator);
+			p.theta = phi + gauss_t(generator);
 		}
 	}
 }
@@ -88,19 +88,15 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
 	//   implement this method and use it as a helper during the updateWeights phase.
 
-
-	for (int i = 0; i < observations.size(); i++) {
+	for (auto& obs : observations) {
 
 		double error = std::numeric_limits<double>::max();
 
 		for (int j = 0; j < predicted.size(); j++){
-
-			const double dx = predicted[j].x - observations[i].x;
-			const double dy = predicted[j].y - observations[i].y;
-			const double current_error = dx*dx + dy*dy;
+			const double current_error = dist(predicted[j].x,  predicted[j].y, obs.x, obs.y);
 
 			if (current_error < error) {
-				observations[i].id = j;
+				obs.id = j;
 				error = current_error;
 			}
 		}
@@ -121,7 +117,43 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33
 	//   http://planning.cs.uiuc.edu/node99.html
 
-	
+	for (int i = 0; i < num_particles; i++) {
+		
+		vector<LandmarkObs> map_observations;
+		for (auto& obs : observations){
+
+			const double tx = particles[i].x + obs.x * cos(particles[i].theta) - obs.y * sin(particles[i].theta);
+			const double ty = particles[i].y + obs.y * cos(particles[i].theta) + obs.x * sin(particles[i].theta);
+
+			LandmarkObs observation = { obs.id, tx, ty};
+			map_observations.push_back(observation);
+		}
+
+		vector<LandmarkObs> landmarks;
+		for (auto& map_mark : map_landmarks.landmark_list) {
+
+			const double distance = dist(particles[i].x, particles[i].y, map_mark.x_f, map_mark.y_f);
+
+			if (distance < sensor_range) {
+				LandmarkObs landmark_in_range = { map_mark.id_i, map_mark.x_f, map_mark.y_f };
+				landmarks.push_back(landmark_in_range);
+			}
+		}
+
+		dataAssociation(landmarks, map_observations);
+
+		particles[i].weight = 1.0;
+
+		for (auto& obs : map_observations) {
+
+			const double a = 0.5 / (pow(std_landmark[0], 2)) * pow(obs.x - landmarks[obs.id].x, 2);
+			const double b = 0.5 / (pow(std_landmark[1], 2)) * pow(obs.y - landmarks[obs.id].y, 2);
+
+			particles[i].weight *= exp(-(a + b)) / sqrt( 2.0 * M_PI * std_landmark[0] * std_landmark[1]);
+		}
+
+		weights[i] = particles[i].weight;		
+	}
 }
 
 void ParticleFilter::resample() {
